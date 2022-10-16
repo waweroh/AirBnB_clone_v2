dd19892ff4e9d8fc48853021b0a5d43357bc1f6c
waweroh <scadmsez@gmail.com>
2022-10-16 23:42:23 +0300

    start flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Start flask """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Returns Hello HBNB """
+    return 'Hello HBNB!'
+
+if __name__ == "__main__":
+    """ My Main Function """
+    app.run(host='0.0.0.0', port=5000)
